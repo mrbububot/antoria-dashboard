156484492594f9acf525ce935a1a6ef3159907ae
mrbububot <mrbububot@gmail.com>
2025-07-14 19:38:17 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -44,7 +44,7 @@ st.markdown("""
         margin-bottom: 1rem;
     }
     input {
-        font-size: 14px !important;
+        font-size: 10px !important;
     }
     .stTextInput > div > input {
         padding: 10px;
